29a5bf44a114c3f6baa653117d9a62b10277186e
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-11 11:26:42 +0900

    Create pro_quantiry_done.php

---
+++ syouhinn/pro_quantiry_done.php
@@ -0,0 +1,41 @@
+<?php
+ require_once('../dbc/sraff_dbc.php');
+    severrq();
+    session();
+    $post=sanitize($_POST);
+    $pro_name=$post['name'];
+    $pro_id=$post['id'];
+    $pro_quantiry=$post['quantiry'];
+    try {
+       
+    
+        
+        $dbh= new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+            ]);
+    }catch (PDOException $e) {
+		exit('データベース接続失敗。'.$e->getMessage());
+	}
+    
+    $stmt = $dbh -> prepare('UPDATE `shop_list` SET `quantiry` = :quantiry WHERE `id`=:id');
+    $stmt -> bindParam(':quantiry',$post['quantiry']);
+    $stmt -> bindParam(':id',$post['id']);
+    $stmt -> execute();
+    $dbh=null;
+    ?>
+    <!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <title>商品個数変更</title>
+</head>
+<body>
+    <div id="wrapper">
+<h1>個数を変更しました</h1>
+    <a href="pro_list.php">戻る</a>
+    </div>
+</body>
+</html>    
