1933bd007c94b3f8719a3159fc18d5df5279b0ec
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-11 11:03:14 +0900

    Create staff_branch.php

---
+++ staff/staff_branch.php
@@ -0,0 +1,36 @@
+require_once('../dbc/sraff_dbc.php');
+
+session();
+
+if(empty($_POST['staff_code'])){
+    
+    if (isset($_POST['add'])) {
+        $staff_code=$_POST['staff_code'];
+        header('Location:staff_add.php?staff_code='.$staff_code);
+        exit();
+    }else{
+    header("Location:staff_ng.php");
+    exit();
+    }
+}else if(isset($_POST['edit'])){
+    
+    $staff_code=$_POST['staff_code'];
+    header('Location:staff_edit.php?staff_code='.$staff_code);
+     exit();
+}elseif(isset($_POST['delete'])){
+
+    $staff_code=$_POST['staff_code'];
+    header('Location:staff_delete.php?staff_code='.$staff_code);
+    exit();
+
+}elseif(isset($_POST['add'])){
+    $staff_code=$_POST['staff_code'];
+    header('Location:staff_add.php?staff_code='.$staff_code);
+    exit();
+
+}else{
+    $staff_code=$_POST['staff_code'];
+    header('Location:staff_disp.php?staff_code='.$staff_code);
+    exit();
+
+};
