bcc2e65f16c1c51a0c80894dff301a97d26ba5a7
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-21 09:37:53 +0900

    Update staff_branch.php

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -1,36 +1,53 @@
+<?php 
+
 require_once('../dbc/sraff_dbc.php');
-
+//ログインしているか確認
 session();
-
-if(empty($_POST['staff_code'])){
+/*
+以下、前画面から送られた、staff_codeを各ページに振り分けていく
+ファイル、swite文の方が見やすいのかも
+*/
+if(empty($_POST['staff_code']))
+{
     
-    if (isset($_POST['add'])) {
+    if (isset($_POST['add'])) 
+    {
         $staff_code=$_POST['staff_code'];
         header('Location:staff_add.php?staff_code='.$staff_code);
         exit();
-    }else{
+    }
+    else
+    {
+        //そもそも指定しなければNGページに飛ばす
     header("Location:staff_ng.php");
     exit();
     }
-}else if(isset($_POST['edit'])){
+}
+else if(isset($_POST['edit']))
+{
     
     $staff_code=$_POST['staff_code'];
     header('Location:staff_edit.php?staff_code='.$staff_code);
      exit();
-}elseif(isset($_POST['delete'])){
+}elseif(isset($_POST['delete']))
+{
 
     $staff_code=$_POST['staff_code'];
     header('Location:staff_delete.php?staff_code='.$staff_code);
     exit();
 
-}elseif(isset($_POST['add'])){
+}elseif(isset($_POST['add']))
+{
     $staff_code=$_POST['staff_code'];
     header('Location:staff_add.php?staff_code='.$staff_code);
     exit();
 
-}else{
+}else
+{
     $staff_code=$_POST['staff_code'];
     header('Location:staff_disp.php?staff_code='.$staff_code);
     exit();
 
 };
+
+?>
