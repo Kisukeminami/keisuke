2c8365ec860931d986db73dbd43881a66fb0045d
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-21 09:18:49 +0900

    Update pro_branch.php

--- syouhinn/pro_branch.php
+++ syouhinn/pro_branch.php
@@ -1,7 +1,9 @@
 <?php 
 var_dump($_POST);
+
 session_start();
 session_regenerate_id(true);
+//ログインしているか確認命令
 if(isset($_SESSION['login'])==false)
 {
         echo "ログインされていません";
@@ -9,46 +11,61 @@ if(isset($_SESSION['login'])==false)
         exit();
     
 };
-if(empty($_POST['procode'])){
-    if(isset($_POST['add'])){
+//procodeがあれば前ページに指定した各ページに飛ばせる
+if(empty($_POST['procode']))
+{
+    if(isset($_POST['add']))
+    {
 
         $pro_code=$_POST['pro_code'];
     
         header('Location:pro_add.php?pro_code='.$pro_code);
         exit();
     
-    }else{
+    }
+    else
+    {
         echo "NG";
         header("Location:pro_ng.php");
         exit();
     }
-}else if(isset($_POST['edit'])){
+}
+else if(isset($_POST['edit']))
+{
     echo "OK";
     $pro_code=$_POST['procode'];
 
     header('Location:pro_edit.php?pro_code='.$pro_code);
      exit();
-}elseif(isset($_POST['delete'])){
+}
+elseif(isset($_POST['delete']))
+{
     echo "lo";
     $pro_code=$_POST['procode'];
 
     header('Location:pro_delete.php?pro_code='.$pro_code);
     exit();
 
-}elseif(isset($_POST['add'])){
+}
+elseif(isset($_POST['add']))
+{
 
     $pro_code=$_POST['procode'];
 
     header('Location:pro_add.php?pro_code='.$pro_code);
     exit();
 
-}elseif(isset($_POST['disp'])){
+}
+elseif(isset($_POST['disp']))
+{
     $pro_code=$_POST['procode'];
 
     header('Location:pro_disp.php?pro_code='.$pro_code);
     exit();
 
-}else{
+}
+else
+{
     $pro_code=$_POST['procode'];
 
     header('Location:pro_quantity.php?pro_code='.$pro_code);
