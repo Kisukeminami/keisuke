1a3efdaec87a76ce2dbc9a1daf42bf30176914f6
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-21 10:13:03 +0900

    Update sraff_dbc.php

--- dbc/sraff_dbc.php
+++ dbc/sraff_dbc.php
@@ -1,55 +1,71 @@
 <?php 
-
-function session(){
+//ログインしているか確認命令
+function session()
+{
     session_start();
     session_regenerate_id(true);
-    if (isset($_SESSION['login'])==false) {
+    if (isset($_SESSION['login'])==false) 
+    {
         echo "ログインされていません";
         echo '<a href="../login/staff_login.php">ログイン画面へ</a>';
         exit();
     }
 }
-
-$dsn='mysql:host=mysql152.phy.lolipop.lan;dbname=LAA1354465-3730kagu;charset=utf8';
-
-$user='LAA1354465';
-
-$pass='Minami3730';
-
-function severrq(){
-    if ($_SERVER["REQUEST_METHOD"]!=="POST") {
+//$dsnにホスト名とDB名、utf8を代入
+$dsn='＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊';
+//ユーザー名代入
+$user='＊＊＊＊＊';
+//パスワードを代入
+$pass='＊＊＊＊＊';
+//POST送信以外のアクセスを防ぐ
+function severrq()
+{
+    if ($_SERVER["REQUEST_METHOD"]!=="POST")
+     {
         echo "不正アクセスです";
         exit();
     }
 }
-function anthority_top(){
-    if($_SESSION['staff_anthority'] >1){
+//スタッフの権限確認、1番強い権限、以下なら別ページに飛ばす
+function anthority_top()
+{
+    if($_SESSION['staff_anthority'] >1)
+    {
         header('location:staff_anthority.html');
         exit();
     }
 }
-
-function anthority_second(){
+//スタッフの権限確認、2番強い権限、以下なら別ページに飛ばす
+function anthority_second()
+{
     if($_SESSION['staff_anthority'] > 2){
         header('location:staff_anthority.html');
         exit();
     }
 }
-function anthority_sarede(){
-    if($_SESSION['staff_anthority'] > 3){
+//スタッフの権限確認、3番強い権限、以下なら別ページに飛ばす
+function anthority_sarede()
+{
+    if($_SESSION['staff_anthority'] > 3)
+    {
         header('location:staff_anthority.html');
         exit();
     }
 }
-function anthority_forse(){
-    if($_SESSION['staff_anthority'] > 4){
+//スタッフの権限確認、3番強い権限、以下なら別ページに飛ばす
+function anthority_forse()
+{
+    if($_SESSION['staff_anthority'] > 4)
+    {
         header('location:staff_anthority.html');
         exit();
     }
 }
-
-function sanitize($before){
-    foreach ($before as $key => $value) {
+//引数の値をエスケープする
+function sanitize($before)
+{
+    foreach ($before as $key => $value) 
+    {
         $after[$key]=htmlspecialchars($value,ENT_QUOTES,'UTF-8');
     }
     return $after;
