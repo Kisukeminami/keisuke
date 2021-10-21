cbd68ad7246bf469870b2c5bfc62f3be2033db9c
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-21 09:51:42 +0900

    Update count.php

--- my/count.php
+++ my/count.php
@@ -1,5 +1,6 @@
 <?php
-header('X-FRAME-OPTIONS: SAMEORIGIN');
+
+
 session_start();
 session_regenerate_id(true);
 require_once('../dbc/sraff_dbc.php');
@@ -7,23 +8,28 @@ var_dump($_POST['max']);
 echo "<br>";
 var_dump($_POST);
 echo "<br>";
-
+//変更したカートの商品個数を新しく$count[]に入れていく
 $max=$post['max'];
-for($i=0;$i<$max;$i++){
+for($i=0;$i<$max;$i++)
+{
     $count[]=$post['count'.$i];
     echo $i;
     var_dump($count);
     echo "<br>";
 
 }
+//カートにの商品削除
 $cart=$_SESSION['cart'];
-for($i=$max;0<=$i;$i--){
-    if(isset($post['delete'.$i])==true){
+for($i=$max;0<=$i;$i--)
+{
+    if(isset($post['delete'.$i])==true)
+    {
         array_splice($cart,$i,1);      
         array_splice($count,$i,1);
 
     }
 }
+
 $_SESSION['cart']=$cart;
 $_SESSION['count']=$count;
 var_dump($_SESSION["cart"]);
