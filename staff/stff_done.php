48e173931e238539cbe9a80e5c0d5135865886c0
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-21 09:48:50 +0900

    Update stff_done.php

--- staff/stff_done.php
+++ staff/stff_done.php
@@ -1,22 +1,27 @@
 <?php 
 require_once('../dbc/sraff_dbc.php');
+//POST送信以外のアクセスを防ぐ
 severrq();
+//ログインしているか確認命令
 session();
+//引数の値をエスケープする
 $post=sanitize($_POST);
 $staff_name = $post['name'];
 $staff_anthority=$post['anthority'];
+//パスワードをハッシュ化
 $staff_pass = password_hash($post['pass'],PASSWORD_DEFAULT);
 
 ?>
 
 <?php 
-try {
+try 
+{
 
     
     $dbh= new PDO($dsn, $user, $pass, [
     PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
         ]);
-        
+//shop_staffに値をDBに保存
 $sql='INSERT INTO shop_staff(name,password,anthority) VALUES (?,?,?)';
 $stmt=$dbh->prepare($sql);
 $date[]=$staff_name;
@@ -25,7 +30,9 @@ $date[]=$staff_anthority;
 $stmt->execute($date);
 
 $dbh=null;
-} catch (PDOException $e) {
+} 
+catch (PDOException $e) 
+{
     echo "接続失敗".$e->getMessage();
     exit();
 }
@@ -38,7 +45,7 @@ $dbh=null;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style1.css">
-    <title>Document</title>
+    <title>スタッフ保存</title>
 </head>
 <body>
     <div id=wrapper>
