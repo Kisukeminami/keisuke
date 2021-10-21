800140778ae2a3386eec8062989c3c907caa530b
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-21 09:45:02 +0900

    Update staff_edit_check.php

--- staff/staff_edit_check.php
+++ staff/staff_edit_check.php
@@ -1,65 +1,89 @@
 <?php
 require_once('../dbc/sraff_dbc.php');
+//ログインしているか確認命令
 session();
+//POST送信以外のアクセスを防ぐ
 severrq();
-try {
+try 
+{
     $pdo = new PDO($dsn,$user,$pass,
     array(PDO::ATTR_EMULATE_PREPARES => false));
 }
 catch (PDOException $e) {
     exit('データベース接続失敗。'.$e->getMessage());
 }
-
+//引数の値をエスケープする
     $post=sanitize($_POST);
     $staff_code=$post['staff_code'];
     $stff_name=$post['name'];
     $stff_pass=$post['pass'];
     $stff_pass2=$post['pass2'];
+    //中身がなければ役職が選択されていませんを代入
     $stff_anthority=$post['anthority'] ?? "役職が選択されていません<br>";
 
     $errors=array();
-
-if((isset($stff_name) && strlen($stff_name))==false){        
+//stff_nameの有無
+if((isset($stff_name) && strlen($stff_name))==false)
+{        
     $errors['name']="スタッフ名が入力されていません";
-}elseif(mb_strlen($stff_name)>15){
+}elseif(mb_strlen($stff_name)>15)
+{
     $errors['name']="スタッフ名が長いです15文字以内でお願いします";
 }
-
+//パスワードの正規表現
 $passmatch="/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{1,11}$/";   
-if ((isset($stff_pass) && strlen($stff_pass))==false) {
+if ((isset($stff_pass) && strlen($stff_pass))==false) 
+{
     $errors['pass']="パスワードが入力されていません";
-}elseif(!preg_match($passmatch,$stff_pass)){
+    //正規表現に会うか
+}
+elseif(!preg_match($passmatch,$stff_pass))
+{
     $errors["pass"]="パスワードを正しく入力してください";
-}elseif(mb_strlen($stff_pass)>11){
+}elseif(mb_strlen($stff_pass)>11)
+{
     $errors["pass"]="パスワードは11文字以内にしてください";
-}else{
+}
+else
+{
     $stmt=$pdo->prepare("SELECT `password` FROM `shop_staff`");
     $stmt->execute();
-    
-    while ($result=$stmt->fetch(PDO::FETCH_ASSOC)) {
-        if (password_verify($stff_pass, $result['password'])) {
+    //パスワードがDBにあるか判定
+    while ($result=$stmt->fetch(PDO::FETCH_ASSOC)) 
+    {
+        if (password_verify($stff_pass, $result['password'])) 
+        {
             $errors["pass"]="このパスワードは既に登録されています";
             break;
-        } else {
-            if ($stff_pass!=$stff_pass2) {
+        } 
+        else 
+        {
+            if ($stff_pass!=$stff_pass2) 
+            {
                 $errors['pass']="パスワードが一致しません";
                 break;
             }
         }
     }
 }
+//stff_anthorityの値によって役職を代入
 if ($stff_anthority === "")
      {
         $errors['anthority']=$stff_anthority;
-    }elseif($stff_anthority === "1"){
+    }elseif($stff_anthority === "1")
+    {
         $anthority="部長";
-    }elseif($stff_anthority === "2"){
+    }elseif($stff_anthority === "2")
+    {
         
         $anthority="課長";
-    }elseif($stff_anthority === "3"){
+    }elseif($stff_anthority === "3")
+    {
         $anthority="社員";
 
-    }else{
+    }
+    else
+    {
         $anthority="非正規";
 
 
