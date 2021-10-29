83ec15539b1b4b7a677e35b023fa380b519c8cc4
Kisukeminami <92131819+Kisukeminami@users.noreply.github.com>
2021-10-29 15:22:17 +0900

    Update shop_cart_list.php

--- my/shop_cart_list.php
+++ my/shop_cart_list.php
@@ -1,51 +1,82 @@
 <?php 
-header('X-FRAME-OPTIONS: SAMEORIGIN');
 require_once('../dbc/sraff_dbc.php');
 session_start();
 session_regenerate_id(true);
-if(isset($_SESSION['menber_login'])==false)
+$_SESSION['cart_ng']=false;
+if(isset($_SESSION['name']))
 {
- function yuser(){
-     echo "ゲストユーザー";
-     echo '<a href="menber_login.html">会員ログイン</a>';
- }
-}else{
-    function yuser()
-    {
-        echo "ようこそ".$_SESSION['menber_name']."様";
-        echo '<a href="menber_logout.html">会員ログイン</a>';
-    }
+  //フラグを立たせる
+   $flag=true;
+   $name=$_SESSION['name'];
+   if(isset($_SESSION[`filename`]))
+   {
+    $img=$_SESSION[`filename`];
+   }else
+   {
+     $img=null;
+   }
 }
 
-//var_dump($_GET['proid']);
-//$pro_code=$_GET['proid'];
+//カートに商品が入っているか
+if(isset($_SESSION['cart'])==false){
+    
+exit("商品がカートに入っていません");
+}
 $cart=$_SESSION['cart'];
-var_dump($cart);
-exit();
+$count=$_SESSION['count'];
+$max=count($cart);
 try {
-   
+    
+
 
     $dbh= new PDO($dsn, $user, $pass, [
     PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
     ]);
-//shop_list,list_detail,sachを結合し、idで紐ずけ
-    $sql ='SELECT * FROM shop_list JOIN list_detail ON shop_list.cood = list_detail.cood JOIN sach ON shop_list.cood = sach.cood WHERE id=?';
-    $stmt=$dbh->prepare($sql);
-    $date[]=$pro_code;
-    $stmt->execute($date);
-    $disp_aitems=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+//カート分だけテーブルから拾ってくる。
+    foreach ($cart as $key=>$value) 
+    {
+        $sql ='SELECT * FROM shop_list JOIN list_detail ON shop_list.cood = list_detail.cood JOIN sach ON shop_list.cood = sach.cood WHERE id=?';
+        $stmt=$dbh->prepare($sql);
+        $date[0]=$value;
+        $stmt->execute($date);
+        $disp_aitems=$stmt->fetch(PDO::FETCH_ASSOC);
 
+        //qrint_r(array_column($disp_aitems,'name'));
+
+    $disp_name[]=$disp_aitems['name']; 
+    $disp_id[]=$disp_aitems['id'];
+    $disp_price[]=$disp_aitems['price']; 
+    $disp_size[]=$disp_aitems['size']; 
+    if( $disp_aitems["category"] === "1"):
+        $disp_ctegory[]="テーブル";
+     elseif($disp_aitems["category"] === "2"):
+        $disp_ctegory[]="イス";
+
+    elseif($disp_aitems["category"] === "3"): 
+        $disp_ctegory[]="ソファー";
+
+     else :
+        $disp_ctegory[]="その他";
+
+      endif ;
+     $disp_image[]=$disp_aitems['file_path'];
+     $disp_quantiry[]=$disp_aitems['quantiry'];
+    }
     
-    $dbh=null;
 
+ 
 
     } catch (PDOException $e) {
     echo "接続失敗".$e->getMessage();
     exit();
 
 }
+$tatalprice=0;
+$dbh=null;
 
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -55,43 +86,60 @@ try {
     <title>商品情報</title>
 </head>
 <body>
+<div id="upmain">
+    <!-- フラグが立っていたら　ユーザーを表示 -->
+  <?php if($flag): ?>
+    <h3><?php echo $name ?>様</h3>
+    <p>いらっしゃいませ</p>
+      <?php if(isset($img)):?>
+       <img src="../yuser/<?php  echo $img?>"width="50px" height="50px">
+      <?php endif; ?>  
+  <?php else: ?> 
+    <h3>ゲストユーザー様</h3>
+    <p>いらっしゃいませ</p>
+    <a href="./login.php">会員ログイン</a>
+  <?php endif; ?>  
+  </div>
+</header>
     <h1>商品情報</h1>
     <br>
-    <?php foreach($disp_aitems as $disp_aitem): ?> 
-   <h3>商品名</h3><?php echo $disp_aitem['name'] ?>
-   <h3>商品価格</h3><?php echo $disp_aitem['price'] ?>
-   <h3>商品説明</h3><?php echo $disp_aitem['text'] ?>
-   <h3>商品サイズ</h3><?php echo $disp_aitem['size'] ?>
-   <h3>メーカー</h3><?php echo $disp_aitem['maker'] ?>
-   <h3>ジャンル</h3><?php echo $disp_aitem['genre'] ?>
-   <h3>カテゴリー</h3><?php 
-    if( $disp_aitem["category"] === "1"):?>
-        <h4>テーブル<h4>
-    <br>
-    <?php elseif($disp_aitem["category"] === "2"): ?>
-        <h4> イス</h4>
-        <br>
+ <form method="post" action="count.php">
+<?php 
 
-    <?php elseif($disp_aitem["category"] === "3"): ?>
-     <h4>ソファー</h4>
-     <br>
+?>
+<!--  カートに入っている商品の表示、在庫がなければ削除と数量、変更を促す-->
+        <?php for($i=0;$i<$max;$i++):?>
+            <?php if($disp_quantiry[$i]>=$count[$i]):?>
+        <h3><?php echo $disp_name[$i] ?></h3>
+        <h3><?php echo $disp_price[$i] ?></h3>
+        <h3><?php echo $disp_size[$i] ?></h3>
+        <img src="../syouhinn/image/<?php echo $disp_image[$i]?>" width="200px" height="200px">
+        <input type="number" name="count<?php echo $i;?>" value="<?php echo $count[$i];?>"max="<?php echo $disp_quantiry[$i]?>">
+        削除：<input type="checkbox" name="delete<?php echo $i ?>"> 
+        <?php 
+        $price=$disp_price[$i]*$count[$i];
+        $tatalprice+=$price ;
+        else:
+        ?>
+        <h3><?php $_SESSION['cart_ng']=true;
+         echo $disp_name[$i]?>は在庫切れです</h3>
+        <img src="../syouhinn/image/<?php echo $disp_image[$i]?>" width="200px" height="200px">
+        <input type="number" name="count<?php echo $i;?>" value="<?php echo $count[$i];?>"max="<?php echo $disp_quantiry[$i]?>">
+        <input type="checkbox" name="delete<?php echo $i ?>"> 
+        <P>こちらの商品の数量を変更するか削除してください</P>
+        <?php 
+        endif;
+        endfor;
+        var_dump($cart); 
+        ?>
+   
+    <h3>カート合計金額：<?php echo $tatalprice ?></h3>
 
-    <?php else :?>
-        <h4>その他</h4>
-        <br>
-     <?php endif ?>
-     <h3>メイン商品画像</h3>
-     <?php echo '<img src="../syouhinn/image/'.$disp_aitem['file_path'].'">' ?>
-     <br>
-     <h3>商品右画像</h3>
-     <?php echo '<img src="../syouhinn/image/'.$disp_aitem['file_path_right'].'">' ?>
-     <br>
-     <h3>商品左画像</h3>
-     <?php echo '<img src="../syouhinn/image/'.$disp_aitem['file_path_left'].'">' ?>
-     <br>
-     <h3>商品後ろ画像</h3>
-     <?php echo '<img src="../syouhinn/image/'.$disp_aitem['file_path_back'].'">' ?>
-     <br>
-     <a href="shop_cart.php?proid=<?php echo $disp_aitem['id']?>">カートに入れる</a>
-<?php var_dump($disp_aitem['id'])?>
-<?php endforeach ?>
+<br>
+<input type="hidden" name="max" value="<?php echo $max ;?>">
+<input type="submit" value="削除or数量変更">
+
+</form>
+
+<a href="shop_by.php">購入</a>
+<a href="list.php">戻る</a>
